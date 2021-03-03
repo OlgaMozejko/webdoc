ceded7378a6d019e3125991698b5e6256109b441
keniaordonez <73820105+keniaordonez@users.noreply.github.com>
2021-03-03 10:10:06 +0100

    Kenia's scroll reveal
    kenia

--- Project2/JS.js
+++ Project2/JS.js
@@ -1,3 +1,24 @@
+//SCROLL REVEAL KENIA
+
+ScrollReveal({
+    duration: 1000
+})
+
+ScrollReveal().reveal('.h1-text-hp');
+
+ScrollReveal().reveal('.nuuii-hp', {
+    delay: 600
+})
+
+ScrollReveal().reveal('.button-hp', {
+    delay: 1000
+})
+
+ScrollReveal().reveal('.arrows-hp', {
+    delay: 1000
+})
+
+
 console.log('JavaScript from js/js.js: up and running!');
 
 
@@ -42,3 +63,5 @@ oreplay.addEventListener('click', restartVid)
 
 var scene = document.getElementsByClassName("illustration-hp");
 var parallaxInstance = new Parallax(illustration-hp);
+
+
